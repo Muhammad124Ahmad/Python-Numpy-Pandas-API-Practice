6f7f93f82be70c3cc23fb3c2f83e845cca7d81c3
muhammad1249ahmad <>
2025-07-29 20:24:28 +0500

    Completed Web Scraping-Lab-23

--- demo.py
+++ demo.py
@@ -1,14 +1,14 @@
 import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 
-URL="https://official-joke-api.appspot.com/jokes/ten"
-jokes=requests.get(URL)
+url = "https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DA0321EN-SkillsNetwork/labs/datasets/HTMLColorCodes.html"
 
-results=jokes.json()
+tables=pd.read_html(url)
+pd.read_jso
+print(len(tables))
 
-df=pd.DataFrame(results)
 
-print(df.head())
 
 
 
@@ -37,20 +37,22 @@ print(df.head())
 
 
 
-# data = requests.get("https://fruityvice.com/api/fruit/all")
 
-# results=data.json()
 
-# df=pd.DataFrame(results)
 
-# df2=pd.json_normalize(results)
+# data=requests.get(url).text
+# table=BeautifulSoup(data,"html5lib")
 
+# Get all rows from the table
+# for row in table.find_all('tr'):  # in html table row represented by tag <tr>
+#     # Get all columns in each row.
+#     cols = row.find_all('td')  # in html a column is represented by tag <td>
+#     color_name = cols[2].string  # store the value in column 3 as color_name
+#     color_code = cols[3].text  # store the value in column 4 as color_code
+#     print("{}--->{}".format(color_name, color_code))
 
-# cherry=df2.loc[df2['name']=='Cherry']
 
 
-# banana=df2.loc[df2['name']=='Banana']
-# print(banana.iloc[0]['nutritions.calories'])
 
 
 
@@ -75,28 +77,47 @@ print(df.head())
 
 
 
+#html = "<!DOCTYPE html><html><head><title>Page Title</title></head><body><h3> \
+# <b id='boldest'>Lebron James</b></h3><p> Salary: $ 92,000,000 </p> \
+# <h3>Stephen Curry</h3><p> Salary: $85,000,000</p> \
+# <h3>Kevin Durant</h3><p> Salary: $73,200,000</p></body></html>"
 
+# table = "<table><tr><td id='flight'>Flight No</td><td>Launch site</td> \
+# <td>Payload mass</td></tr><tr> <td>1</td> \
+# <td id='yes'><a href='https://en.wikipedia.org/wiki/Florida'>Florida<a></td> \
+# <td>300 kg</td></tr><tr><td>2</td> \
+# <td><a href='https://en.wikipedia.org/wiki/Texas'>Texas</a></td> \
+# <td>94 kg</td></tr><tr><td>3</td> \
+# <td><a href='https://en.wikipedia.org/wiki/Florida'>Florida<a> </td> \
+# <td>80 kg</td></tr></table>"
 
 
+# two_tables="<h3>Rocket Launch </h3> \
+# <p><table class='rocket'> \
+# <tr><td>Flight No</td><td>Launch site</td><td>Payload mass</td></tr> \
+# <tr><td>1</td><td>Florida</td><td>300 kg</td></tr> \
+# <tr><td>2</td><td>Texas</td><td>94 kg</td></tr> \
+# <tr><td>3</td><td>Florida </td><td>80 kg</td></tr></table></p>\
+# <p><h3>Pizza Party</h3> \
+# <table class='pizza'> \
+# <tr><td>Pizza Place</td><td>Orders</td><td>Slices </td></tr> \
+# <tr><td>Domino's Pizza</td><td>10</td><td>100</td></tr> \
+# <tr><td>Little Caesars</td><td>12</td><td >144 </td></tr> \
+# <tr><td>Papa John's</td><td>15 </td><td>165</td></tr>"
 
+# two_tables_bs=BeautifulSoup(two_tables,'html.parser')
 
+# secondTable=two_tables_bs.find('table',class_='pizza')
+# print(secondTable.prettify())
 
+# url = "http://www.ibm.com"
+# data=requests.get(url).text
 
+# soup = BeautifulSoup(data, "html5lib")
 
+# for link in soup.find_all('a',href=True):
+#   print(link.get('href'))
 
-
-# from randomuser import RandomUser
-# import pandas as pd
-
-
-
-
-# def get_users():
-#     users =[]
-     
-#     for user in RandomUser.generate_users(10):
-#         users.append({"Name":user.get_full_name(),"Gender":user.get_gender(),"City":user.get_city(),"State":user.get_state(),"Email":user.get_email(), "DOB":user.get_dob(),"Picture":user.get_picture()})
-      
-#     return pd.DataFrame(users)   
-
-# print(get_users())   
+# for link in soup.find_all('img',href=True):
+#   print(link.get('src'))  
+  
