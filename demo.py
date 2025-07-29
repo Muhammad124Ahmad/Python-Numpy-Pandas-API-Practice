eaa03d4b9c5e847f1ec8b0a2b14a3730b45d3eee
muhammad1249ahmad <>
2025-07-29 15:56:34 +0500

    Completed HTTP AND REQUESTS-LAB-21

--- demo.py
+++ demo.py
@@ -1,21 +1,15 @@
-import pandas as pd
-import numpy as np
+import os
+import requests
 
-tables=pd.read_html("https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29")
 
-df=tables[3]
 
-# table_3.columns=range(table_3.shape[1])
-df.columns=range(df.shape[1])
-df=df[[0,2]]
 
-df=df.iloc[1:11,:]
+URL="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/Example1.txt"
 
-df.columns=['Country','GDP (MILLION USD)']
+r=requests.get(URL)
 
-df['GDP (MILLION USD)']=df['GDP (MILLION USD)'].astype(int)
-df['GDP (MILLION USD)']=df[['GDP (MILLION USD)']]/1000
-df['GDP (MILLION USD)']=np.round(df['GDP (MILLION USD)'],2)
-df.rename(columns={"GDP (MILLION USD)":"GDP (BILLION USD)"},inplace=True)
+path=os.path.join(os.getcwd(),'Example.txt')
 
-df.to_csv("Largest_economies.csv")
+with open(path,'wb') as file:
+  file.write(r.content)
+   
