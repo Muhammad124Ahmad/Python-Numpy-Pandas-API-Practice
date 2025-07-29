ec126e4256c1f60e025c82a4ec2e47c8bbd36415
muhammad1249ahmad <>
2025-07-29 18:20:06 +0500

    Completed LAB-22-API-EXAMPLES

--- demo.py
+++ demo.py
@@ -1,15 +1,102 @@
-import os
 import requests
+import pandas as pd
+
+URL="https://official-joke-api.appspot.com/jokes/ten"
+jokes=requests.get(URL)
+
+results=jokes.json()
+
+df=pd.DataFrame(results)
+
+print(df.head())
 
 
 
 
-URL="https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%205/data/Example1.txt"
 
-r=requests.get(URL)
 
-path=os.path.join(os.getcwd(),'Example.txt')
 
-with open(path,'wb') as file:
-  file.write(r.content)
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# data = requests.get("https://fruityvice.com/api/fruit/all")
+
+# results=data.json()
+
+# df=pd.DataFrame(results)
+
+# df2=pd.json_normalize(results)
+
+
+# cherry=df2.loc[df2['name']=='Cherry']
+
+
+# banana=df2.loc[df2['name']=='Banana']
+# print(banana.iloc[0]['nutritions.calories'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from randomuser import RandomUser
+# import pandas as pd
+
+
+
+
+# def get_users():
+#     users =[]
+     
+#     for user in RandomUser.generate_users(10):
+#         users.append({"Name":user.get_full_name(),"Gender":user.get_gender(),"City":user.get_city(),"State":user.get_state(),"Email":user.get_email(), "DOB":user.get_dob(),"Picture":user.get_picture()})
+      
+#     return pd.DataFrame(users)   
+
+# print(get_users())   
